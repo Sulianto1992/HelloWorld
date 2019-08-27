6c4d304c4b28d0ae28450797310edf804d90bd87
Sulianto1992 <Sulianto1992@gmail.com>
2019-08-27 16:57:21 +0800

    Docker Hello World Project

---
+++ HelloWorld/src/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package demo; //Name of the package
+
+public class HelloWorld //Name of the class 
+{
+	public static void main(String[] args) //Start of the program
+	{
+		System.out.println("Hello World!"); //Output of the program
+	} //end main
+	
+} //end class
